cf9591810668d65deb9e5168f23b95a11b4b549d
ooktay <ooktay@OZGUR-OKTAY.europe.nortel.com.>
2011-04-04 17:30:44 +0300

    move @anywhere.js into <head/> in home.jsp. use '/" consistently in index.js.

--- WEB-INF/jsp/home.jsp
+++ WEB-INF/jsp/home.jsp
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Twitstreet</title>
+<script type="text/javascript" src="/js/index.js"></script>
 </head>
 <body>
 
@@ -10,6 +11,5 @@
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 <script src="http://platform.twitter.com/anywhere.js?id=DfIgLzNr6zl8gcE4sVFFgQ&v=1" type="text/javascript"></script>
-<script type="text/javascript" src="/js/index.js"></script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ $(document).ready(
 					currentUser = T.currentUser;
 					screenName = currentUser.data('screen_name');
 					profileImage = currentUser.data('profile_image_url');
-					profileImageTag = "<img src='" + profileImage + "'/>";
+					profileImageTag = '<img src="' + profileImage + '"/>';
 					$('#tcp').append(
-							"Logged in as " + profileImageTag + " "
+							'Logged in as ' + profileImageTag + ' '
 									+ screenName);
 				} else {
-					T("#tcp").connectButton();
+					T('#tcp').connectButton();
 				}
 			}
 
