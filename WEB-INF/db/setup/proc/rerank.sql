2f6f537ebd1d99556094b2b2121594a044f4196b
Engin Guller <eguller@ENGIN-GULLER.netas.lab.nortel.com>
2012-01-20 16:39:13 +0200

    fix error in buy

--- WEB-INF/db/setup/proc/rerank.sql
+++ WEB-INF/db/setup/proc/rerank.sql
@@ -2,26 +2,26 @@ drop procedure if exists rerank;
 delimiter $$
 create procedure rerank()
 begin
-	declare done int default false;
-	declare  user_id int;
-	declare  userRank int;
-	declare  new_rank  int default 1;
-	declare cur1 cursor for select id, rank from users order by portfolio_value(id) desc;
-	declare continue handler for not found set done = true;
-	open cur1;
+    declare done int default false;
+    declare  user_id int;
+    declare  userRank int;
+    declare  new_rank  int default 1;
+    declare cur1 cursor for select id, rank from users order by portfolio_value(id) desc;
+    declare continue handler for not found set done = true;
+    open cur1;
     set @new_rank := 1;
-	read_loop: LOOP
+    read_loop: LOOP
     fetch cur1 into user_id, userRank;   
-    	if done then
-      		leave read_loop;
-    	end if;   
-    	if @new_rank > userRank then
-			update users set rank = @new_rank, direction = 1 where id = user_id;
-		else
-			update users set rank = @new_rank, direction = 0 where id = user_id;
-		end if;
-		set @new_rank := @new_rank + 1;
-  	end loop read_loop;	
-  	close cur1;
+        if done then
+            leave read_loop;
+        end if;   
+        if @new_rank > userRank then
+            update users set rank = @new_rank, direction = 1 where id = user_id;
+        else
+            update users set rank = @new_rank, direction = 0 where id = user_id;
+        end if;
+        set @new_rank := @new_rank + 1;
+    end loop read_loop;    
+    close cur1;
 end $$
 delimiter ; 
