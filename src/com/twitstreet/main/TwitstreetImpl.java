867eb5e43be26c62da9aa5b2d370e1249ad2eed5
mushanga <matkap@gmail.com>
2012-02-01 04:13:28 +0200

    remove comment line

--- src/com/twitstreet/main/TwitstreetImpl.java
+++ src/com/twitstreet/main/TwitstreetImpl.java
@@ -76,7 +76,7 @@ public class TwitstreetImpl implements Twitstreet {
 		
 		Thread updateFollowerCountThread = new Thread (updateFollowerCountTask);
 		updateFollowerCountThread.setName("Update Follower Count");
-		//updateFollowerCountThread.start();
+		updateFollowerCountThread.start();
 		
 		Thread asyncQueryTaskThread = new Thread(asyncQueryTask);
 		asyncQueryTaskThread.setName("Async query task");
@@ -84,7 +84,7 @@ public class TwitstreetImpl implements Twitstreet {
 		
 		Thread stockHistoryUpdateThread = new Thread (stockHistoryUpdateTask);
 		stockHistoryUpdateThread.setName("Update Stock History");
-		//stockHistoryUpdateThread.start();
+		stockHistoryUpdateThread.start();
 		
 		transactionCache.load();
 		
