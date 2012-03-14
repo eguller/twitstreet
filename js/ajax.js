7c8c054a561cfd4937919c590be02bed951d4ed2
Engin Guller <bisanthe@gmail.com>
2012-03-14 11:01:59 +0200

    fix user stock search fix user profile update task

--- js/ajax.js
+++ js/ajax.js
@@ -55,23 +55,15 @@ function timeToReload(lastUpdate){
 
 var lastCommand = null;
 $(function(){
-	  
 	  // Bind an event to window.onhashchange that, when the hash changes, gets the
 	  // hash and adds the class "selected" to any matching nav link.
 	  $(window).hashchange( function(){
 	    var hash = location.hash;
-	    
 	    // Set the page title based on the hash.
 	    var command =  hash.replace( /^#/, '' );
-	    
-
 	    command = command.replace('!','');
-	    
 	    lastCommand = command;
-	    
 	    performOperation(command);
-	    
-	 
 	  })
 	  
 	  // Since the event is only triggered when the hash changes, we need to trigger
@@ -85,74 +77,50 @@ function performLastOperation(){
 }
 function performOperation(command){
 	var itemType = command.split('=')[0];
-		    
     if(itemType == 'stock'){
     	 var id = command.split('=')[1];
     	loadStock(id);
-    	
     }else if(itemType == 'user'){
     	 var id = command.split('=')[1];
     	loadUserProfile(id);
     }
 	else if(itemType == 'suggestedstocks'){
-    	
     	loadTrendyStocks();
     }else if(itemType == 'topgrossingusers'){
-    	
     	loadTrendyUsers();
     }else if(itemType == 'searchstock'){
-
-    	var searchString = command.split('-')[1];
+    	var searchString = command.split('=')[1];
     	getQuote(searchString);
     }
     else if(itemType == 'searchuser'){
-
-    	var searchString = command.split('-')[1];
+    	var searchString = command.split('=')[1];
     	getUser(searchString);
     }
 }
 function loadLanguage(lang){
-	
 	ajaxLoad("/lang", 'lang='+lang, null, "body", false, reloadPage);
-	
 }
 function reloadPage(data){
-	
 	location.reload(true);
 	if(data!=null && data.length>0){
-		
-
 		blockElementWithMsg("body", data);
 	}
 }
 
 function reloadIfHashIsMyHref(element){
-	
 	if(isLocationEqualTo($(element).attr('href'))) performLastOperation();
-	
-	
 }
 
 function reloadIfHashIs(hash){
-	
 	if(isLocationEqualTo(hash)) performLastOperation();
-	
-	
 }
 
 function isLocationEqualTo(url){
-	
-	
 	return window.location.hash==url;
-	
 }
-
 
 function widgetLoad(){
 	if(twttr.widgets){
 		twttr.widgets.load();
 	}
-	
-	
-	
 }
