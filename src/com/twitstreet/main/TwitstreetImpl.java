1abb88864adb584388e90d81542659b229b5a668
mushanga <matkap@gmail.com>
2012-03-25 04:46:56 +0300

    - remove tasks from master server

--- src/com/twitstreet/main/TwitstreetImpl.java
+++ src/com/twitstreet/main/TwitstreetImpl.java
@@ -59,7 +59,7 @@ public class TwitstreetImpl implements Twitstreet {
 		
 		loadSeasonInfo();
 	
-		if (!configMgr.isMaster()) {
+		if (!configMgr.isMaster() || configMgr.isDev()) {
 			startTasks();
 
 		}
