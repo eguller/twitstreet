f8375c7c00f58de87e813f5acc30948c692da05e
Engin Guller <bisanthe@gmail.com>
2012-04-09 00:48:19 +0300

    #161

--- src/com/twitstreet/twitter/TwitstreetAnnouncer.java
+++ src/com/twitstreet/twitter/TwitstreetAnnouncer.java
@@ -1,15 +1,27 @@
+/**
+	TwitStreet - Twitter Stock Market Game
+    Copyright (C) 2012  Engin Guller (bisanth@gmail.com), Cagdas Ozek (cagdasozek@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package com.twitstreet.twitter;
 
 import com.twitstreet.db.data.Stock;
 
-
-
 public interface TwitstreetAnnouncer {
-	
-	
-	
 	public boolean mention(Stock user, String message);
-
 	public void removeOldRecords(int removeOlderThanMinutes);
 	public void removeOldRecordsByServer(int removeOlderThanMinutes);
 	
