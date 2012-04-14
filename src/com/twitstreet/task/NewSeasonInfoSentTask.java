9a3f23fd3d418225f3b28633e3165b12122e62e2
Engin Guller <bisanthe@gmail.com>
2012-04-14 17:53:53 +0300

    #181

--- src/com/twitstreet/task/NewSeasonInfoSentTask.java
+++ src/com/twitstreet/task/NewSeasonInfoSentTask.java
@@ -2,6 +2,8 @@ package com.twitstreet.task;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.google.inject.Inject;
 import com.twitstreet.db.data.User;
 import com.twitstreet.localization.LocalizationUtil;
@@ -10,6 +12,7 @@ import com.twitstreet.twitter.AnnouncerMgr;
 import com.twitstreet.util.Util;
 
 public class NewSeasonInfoSentTask implements Runnable {
+	private static Logger logger = Logger.getLogger(NewSeasonInfoSentTask.class);
 	private static final int SEASONINFO_SIZE = 3;
 	private static final long ONEHOUR = 1 * 60 * 60 * 1000;
 	private static final int SIZE = 1;
@@ -20,9 +23,9 @@ public class NewSeasonInfoSentTask implements Runnable {
 	@Override
 	public void run() {
 		while(true){
-			
 			List<User> userList = userMgr.getNewSeasonInfoNotSentUsers(SIZE);
 			userMgr.setNewSeasonInfoSent(userList);
+			logger.info("New season info sent task started. User count : " + userList.size());
 			for(User user : userList){
 				int seasonInfoMessageId = (int)( Math.random() * SEASONINFO_SIZE );
 				String message = lutil.get("seasoninfo." + seasonInfoMessageId, user.getLanguage());
@@ -33,6 +36,7 @@ public class NewSeasonInfoSentTask implements Runnable {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			logger.info("New season info sent task completed");
 		}
 	}
 
