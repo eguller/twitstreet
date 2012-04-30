f1db9f86ae6a0fc5f629f42e59ddd0c59453ce2e
Engin Guller <bisanthe@gmail.com>
2012-04-30 12:57:19 +0300

    fix error in advertisement task

--- src/com/twitstreet/twitter/AdsListenerMgrImpl.java
+++ src/com/twitstreet/twitter/AdsListenerMgrImpl.java
@@ -34,7 +34,7 @@ public class AdsListenerMgrImpl implements AdsListenerMgr {
 	private static final int REGULAR_TWEET = 0;
 	private static final int RETWEEET = 1;
 	private static final int FAVOURITE = 2;
-	private static final int ACTION_TYPES = 3;
+	private static final int ACTION_TYPES = 5; // 3 and 4 reserved for default task
 	
 	@Inject AnnouncerMgr announcerMgr;
 	/* (non-Javadoc)
