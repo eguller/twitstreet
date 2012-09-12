555e1f91f4293ed856b020b03d5f15c786189215
Engin Guller <bisanthe@gmail.com>
2012-09-12 14:15:50 +0300

    tomcat6

--- prod-build.sh
+++ prod-build.sh
@@ -2,6 +2,6 @@ cd /root/twitstreet
 /usr/bin/git pull
 /etc/init.d/tomcat6 stop
 rm -rf /var/lib/tomcat6/webapps/ROOT
-ant -Ddest-file=/var/lib/tomcat6/webapps/ROOT.war
+ant -Ddest-file=/var/lib/tomcat6/webapps/ROOT.war -Dtomcat.home=/usr/share/tomcat6
 /etc/init.d/tomcat6 start
 
